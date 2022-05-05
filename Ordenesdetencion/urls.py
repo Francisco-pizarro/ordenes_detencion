35c1ec69c82c7dd4aafba8e582af3c5dfc727e87
Francisco <fpizarro@bluelatam.com>
2022-05-04 21:33:04 -0400

    template

--- Ordenesdetencion/urls.py
+++ Ordenesdetencion/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from.views import index
+from.views import index,resultado
 urlpatterns = [
     path('', index, name= 'inicio'),
+    path('resultado', resultado, name= 'resultado'),
     
 ] 
--- Ordenesdetencion/views.py
+++ Ordenesdetencion/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def resultado(request):
+    return render(request, 'resultado.html')
