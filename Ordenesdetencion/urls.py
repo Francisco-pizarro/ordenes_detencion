b3e418149fe377be26054ba5de692676747956ee
cristiansavaria <cristian2012sa@gmail.com>
2022-05-04 21:48:10 -0400

    se agrega extends paginas

--- Ordenesdetencion/urls.py
+++ Ordenesdetencion/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from.views import index,resultado
 urlpatterns = [
     path('', index, name= 'inicio'),
-    path('resultado', resultado, name= 'resultado'),
+    path('resultado/', resultado, name= 'resultado'),
     
 ] 
