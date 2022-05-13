465b698df9c08184616647bfbc7956826a358a01
Csavariaa <cr.savaria@duocuc.cl>
2022-05-13 00:57:22 -0400

    se agrega admin para modelos en admin

--- Ordenesdetencion/admin.py
+++ Ordenesdetencion/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from Ordenesdetencion.models import Actividad
+from Ordenesdetencion.models import Actividad, Comuna, Delito, EstadoCivil, MedidaCautelar, Orden, Pais, Persona, Region, Sexo, Tribunal
 
 
 admin.site.register(Actividad)
+admin.site.register(Comuna)
+admin.site.register(Delito)
+admin.site.register(EstadoCivil)
+admin.site.register(MedidaCautelar)
+admin.site.register(Pais)
+admin.site.register(Persona)
+admin.site.register(Region)
+admin.site.register(Sexo)
+admin.site.register(Tribunal)
