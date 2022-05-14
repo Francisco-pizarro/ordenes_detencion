52076cee103d5048db5fdf042af959eeeb5cea29
DarienRiquelmee <dari.carrasco@duocuc.cl>
2022-05-14 08:44:32 -0400

    se agregan nombres representativos a las tablas

--- Ordenesdetencion/models.py
+++ Ordenesdetencion/models.py
@@ -22,81 +22,14 @@ class Actividad(models.Model):
         db_table = 'actividad'
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
 class Comuna(models.Model):
     id_comuna = models.AutoField(db_column='ID_COMUNA', primary_key=True)  # Field name made lowercase.
     gls_comuna = models.CharField(db_column='GLS_COMUNA', max_length=50)  # Field name made lowercase.
     gls_rpovincia = models.CharField(db_column='GLS_RPOVINCIA', max_length=50)  # Field name made lowercase.
     region_id_region = models.ForeignKey('Region', models.DO_NOTHING, db_column='REGION_ID_REGION')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_comuna
     class Meta:
         managed = False
         db_table = 'comuna'
@@ -106,69 +39,24 @@ class Delito(models.Model):
     id_delito = models.AutoField(db_column='ID_DELITO', primary_key=True)  # Field name made lowercase.
     gls_delito = models.CharField(db_column='GLS_DELITO', max_length=250)  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_delito
+    
     class Meta:
         managed = False
         db_table = 'delito'
 
 
-class Delitos(models.Model):
-    id_delito = models.IntegerField(db_column='ID_DELITO', blank=True, null=True)  # Field name made lowercase.
-    gls_delito = models.TextField(db_column='GLS_DELITO', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'delitos'
 
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
 
 
 class EstadoCivil(models.Model):
     id_estado_civil = models.AutoField(db_column='ID_ESTADO_CIVIL', primary_key=True)  # Field name made lowercase.
     gls_estado_civil = models.CharField(db_column='GLS_ESTADO_CIVIL', max_length=50)  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_estado_civil
     class Meta:
         managed = False
         db_table = 'estado_civil'
@@ -183,6 +71,9 @@ class MedidaCautelar(models.Model):
     persona_id_persona = models.ForeignKey('Persona', models.DO_NOTHING, db_column='PERSONA_ID_PERSONA')  # Field name made lowercase.
     tribunal_id_tribunal = models.ForeignKey('Tribunal', models.DO_NOTHING, db_column='TRIBUNAL_ID_TRIBUNAL')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.ruc
+    
     class Meta:
         managed = False
         db_table = 'medida_cautelar'
@@ -197,6 +88,9 @@ class Orden(models.Model):
     persona_id_persona = models.ForeignKey('Persona', models.DO_NOTHING, db_column='PERSONA_ID_PERSONA')  # Field name made lowercase.
     tribunal_id_tribunal = models.ForeignKey('Tribunal', models.DO_NOTHING, db_column='TRIBUNAL_ID_TRIBUNAL')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.ruc
+    
     class Meta:
         managed = False
         db_table = 'orden'
@@ -206,6 +100,9 @@ class Pais(models.Model):
     id_pais = models.AutoField(db_column='ID_PAIS', primary_key=True)  # Field name made lowercase.
     gls_pais = models.CharField(db_column='GLS_PAIS', max_length=50)  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_pais
+    
     class Meta:
         managed = False
         db_table = 'pais'
@@ -227,6 +124,9 @@ class Persona(models.Model):
     estado_civil_id_estado_civil = models.ForeignKey(EstadoCivil, models.DO_NOTHING, db_column='ESTADO_CIVIL_ID_ESTADO_CIVIL')  # Field name made lowercase.
     sexo_id_sexo = models.ForeignKey('Sexo', models.DO_NOTHING, db_column='SEXO_ID_SEXO')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_nombres
+    
     class Meta:
         managed = False
         db_table = 'persona'
@@ -237,6 +137,9 @@ class Region(models.Model):
     gls_region = models.CharField(db_column='GLS_REGION', max_length=50)  # Field name made lowercase.
     pais_id_pais = models.ForeignKey(Pais, models.DO_NOTHING, db_column='PAIS_ID_PAIS')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_region
+    
     class Meta:
         managed = False
         db_table = 'region'
@@ -246,6 +149,9 @@ class Sexo(models.Model):
     id_sexo = models.AutoField(db_column='ID_SEXO', primary_key=True)  # Field name made lowercase.
     gls_sexo = models.CharField(db_column='GLS_SEXO', max_length=50)  # Field name made lowercase.
 
+    
+    def __str__(self):
+        return self.gls_sexo
     class Meta:
         managed = False
         db_table = 'sexo'
@@ -256,6 +162,9 @@ class Tribunal(models.Model):
     gls_tribunal = models.CharField(db_column='GLS_TRIBUNAL', max_length=45)  # Field name made lowercase.
     comuna_id_comuna = models.ForeignKey(Comuna, models.DO_NOTHING, db_column='COMUNA_ID_COMUNA')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.gls_tribunal
+    
     class Meta:
         managed = False
         db_table = 'tribunal'
