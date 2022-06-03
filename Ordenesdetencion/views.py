e0408f5d1d2890de69912a21127c853f8deb1a2a
promanv <pablo.roman.villablanca@gmail.com>
2022-06-03 17:49:31 -0400

    ultimos cambios, las 3 vistas funcionando

--- Ordenesdetencion/views.py
+++ Ordenesdetencion/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Persona, Orden
+from .models import Persona, Orden, MedidaCautelar
 
 # Create your views here.
 def index(request):
@@ -7,13 +7,23 @@ def index(request):
     
     if request.method == 'POST':
         rut = request.POST.get('txtRut')
-        if rut:        
-            print('rut1:-'+rut+'-')        
-            #rut='17123213-9'
-            persona = Persona.objects.filter(gls_rut=rut)[0]
-            print('id_persona: '+str(persona.id_persona))
+        if rut:      
+            # 17123213-9 OD
+            # 12223432-4 MC
+            try:
+                persona = Persona.objects.get(gls_rut=rut)
+            except Persona.DoesNotExist:
+                return render(request, 'verde.html')           
+
             orden = Orden.objects.filter(persona_id_persona=persona.id_persona)
-            return render(request, 'rojo.html', {'persona': persona,'orden': orden})
+            cautelar = MedidaCautelar.objects.filter(persona_id_persona=persona.id_persona)
+            if orden:
+                return render(request, 'rojo.html', {'persona': persona,'orden': orden})
+            else:                
+                if cautelar:
+                    return render(request, 'amarillo.html', {'persona': persona,'cautelar': cautelar})
+                else:
+                    return render(request, 'verde.html')            
         else:
             return render(request, 'index.html')            
     else:        
