fe1c318b76fc364724612f5ea4ad5c62ea36a8c2
promanv <pablo.roman.villablanca@gmail.com>
2022-06-03 11:25:45 -0400

    mejoras portada

--- Ordenesdetencion/views.py
+++ Ordenesdetencion/views.py
@@ -13,7 +13,7 @@ def index(request):
             persona = Persona.objects.filter(gls_rut=rut)[0]
             print('id_persona: '+str(persona.id_persona))
             orden = Orden.objects.filter(persona_id_persona=persona.id_persona)
-            return render(request, 'resultado.html', {'persona': persona,'orden': orden})
+            return render(request, 'rojo.html', {'persona': persona,'orden': orden})
         else:
             return render(request, 'index.html')            
     else:        
