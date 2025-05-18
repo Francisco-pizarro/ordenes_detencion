13c5bb441e5b884f7c104b9c9570d6a4940a0547
Francisco pizarro <fco.pzarro23@gmail.com>
2025-05-18 16:49:42 -0400

    add test valida_rut

---
+++ Ordenesdetencion/tests/test_valida_rut.py
@@ -0,0 +1,23 @@
+import pytest
+from Ordenesdetencion.views import validaRut
+
+@pytest.mark.parametrize("rut, esperado", [
+    ("12.345.678-5", True),
+    ("12345678-5", True),
+    ("algasd123", False),
+    ("00.000.000-0", False),
+    ("11.111.111-1", False),
+    ("17937114-6", True),
+    ("abc-def-gh", False),
+    ("1111111111", False)
+])
+def test_validaRut(rut, esperado):
+    resultado = validaRut(rut)
+    if resultado == esperado:
+        print(f"Test PASÓ → RUT: {rut} | Esperado: {esperado} | Resultado: {resultado}")
+    else:
+        assert False
+        
+        
+#pytest -s para visualizar los print expuesto en el codigo
+#pytest solamente devulve resultado del test
