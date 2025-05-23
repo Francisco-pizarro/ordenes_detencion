01748c047671a281c06ca7e294da13344da8ef38
FRANCISCO ISAIAS PIZARRO LEIVA <francisco.pizarro@serviciomigraciones.cl>
2025-05-23 18:43:32 -0400

    add test equivalencia

--- tests/test_cierre_sesion.py
+++ tests/test_cierre_sesion.py
@@ -4,23 +4,25 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @pytest.mark.django_db
+# TEST Equivalencia usuario autenticado
 def test_logout_protege_index(client):
-    # Crear usuario
-    email = 'test@test.com'
+    # Crear usuario              var         clase equivalencia     estado     represenntante 
+    email = 'test@test.com'  #Autenticación	 Usuario autenticado	Válido	Login con email y password
     password = '12345678'
     User.objects.create_user(email=email, password=password)
 
-    # Simular login (sin pasar por tu vista)
+    # Simular login (sin pasar por view login)
     assert client.login(email=email, password=password)
 
     # Acceder a la vista protegida
-    response = client.get('/')
+    response = client.get('/')         # Acceso permitido a vista protegida si logueado	es=	response.status_code == 200 
     assert response.status_code == 200
 
     # Hacer logout manualmente
-    client.logout()
+    client.logout() #Autenticación	Usuario deslogueado	Inválido	Logout manual
 
     # Intentar acceder nuevamente
     response = client.get('/')
     assert response.status_code == 302
-    assert '/login' in response.url  # redirige a login porque ya no hay sesión
+    assert '/login' in response.url  # Vista protegida	Redirección si no hay sesión , Redirección	response.status_code == 302
+
--- tests/test_valida_rut.py
+++ tests/test_valida_rut.py
@@ -2,14 +2,14 @@ import pytest
 from Ordenesdetencion.views import validaRut
 
 @pytest.mark.parametrize("rut, esperado", [
-    ("12.345.678-5", True),
-    ("12345678-5", True),
-    ("algasd123", False),
-    ("00.000.000-0", False),
-    ("11.111.111-1", False),
-    ("17937114-6", True),
-    ("abc-def-gh", False),
-    ("1111111111", False)
+    ("12.345.678-5", True),  # rut	RUT válido con puntos y guion	Válido	12.345.678-5
+    ("12345678-5", True),  # rut	RUT válido sin puntos, con guion	Válido	12345678-5
+    ("algasd123", False),  # rut	Cadena no numérica	Inválido	algasd123
+    ("00.000.000-0", False), # rut	RUT con ceros repetidos	Inválido	00.000.000-0
+    ("11.111.111-1", False), # rut	RUT con valores repetidos	Inválido	11.111.111-1
+    ("17937114-6", True), # rut	RUT válido sin puntos ni guion	Válido	17937114-6
+    ("abc-def-gh", False), # rut	Texto completamente inválido	Inválido	abc-def-gh
+    ("1111111111", False) # rut	Número excesivo sin guion	Inválido	1111111111
 ])
 def test_validaRut(rut, esperado):
     resultado = validaRut(rut)
